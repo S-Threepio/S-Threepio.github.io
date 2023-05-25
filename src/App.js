6e8ba2cf936d57882b80a1415bc2353f7b71b9a7
cult_mentality <kavitkarswanand5@gmail.com>
2023-05-24 20:24:20 -0700

    fixed scaling

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ const App = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <div className="flex flex-1 flex-row">
+      <div className="flex flex-1 flex-row overflow-hidden w-screen relative">
         <div className="flex flex-[2] bg-[#F4ECE6]"></div>
         <div className="flex flex-[3] bg-white "></div>
-        <div className="flex flex-[4] flex-row items-center absolute  top-[20%] left-[30%]">
+        <div className="MyInfo flex flex-[4] flex-row items-center absolute top-[20%] left-[30%] scale-105">
           <Intro />
           <Bio />
         </div>
