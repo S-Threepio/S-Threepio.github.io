0805203fda36ac9083f9b5ac2fcf621a8c93511d
cult_mentality <kavitkarswanand5@gmail.com>
2023-05-26 12:41:17 -0700

    responsive till 768px

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
       <div className="flex flex-row flex-1 w-screen overflow-hidden">
         <div className="flex flex-[2] lg:bg-[#F4ECE6]"></div>
         <div className="flex flex-[3] bg-white "></div>
-        <div className="flex flex-[4] flex-col sm:flex-row justify-center md:left-[10%] items-center md:absolute top-[20%] lg:left-[19%] tablet:left-[20%]  xl:left-[30%] scale-105">
+        <div className="flex flex-[4] flex-col items-center sm:flex-row justify-center pad:left-[10%] pad:absolute top-[20%] lg:left-[19%] tablet:left-[20%]  xl:left-[30%] scale-105">
           <CardSection />
           <Bio />
         </div>
