2645577b9692ae73b27d870e080b79cfa4d55073
cult_mentality <kavitkarswanand5@gmail.com>
2023-05-28 16:27:52 -0700

    fixed button urls

--- src/BasicButtons.js
+++ src/BasicButtons.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const BasicButtons = () => {
+  const handleClick = (url) => {
+    console.log("myurl" + url);
+  };
+
   return (
     <div className="flex flex-row gap-4 my-6 text-base font-semibold md:gap-2 md:text-md">
-      <button className="px-12  py-0.5  bg-white transition duration-100 ease-out hover:scale-105 hover:bg-[#F4ECE6] text-gray-800 border border-gray-400 rounded-full shadow">
+      <Link
+        to="/resume"
+        className="px-12  py-0.5  bg-white transition duration-100 ease-out hover:scale-105 hover:bg-[#F4ECE6] text-gray-800 border border-gray-400 rounded-full shadow"
+      >
         Resume
-      </button>
-      <button className=" px-12  py-0.5  bg-white hover:bg-[#F4ECE6] transition duration-100 text-gray-800 border hover:scale-105 border-gray-400 rounded-full shadow">
+      </Link>
+      <Link
+        to="/projects"
+        className="px-12  py-0.5  bg-white transition duration-100 ease-out hover:scale-105 hover:bg-[#F4ECE6] text-gray-800 border border-gray-400 rounded-full shadow"
+      >
         Projects
-      </button>
+      </Link>
     </div>
   );
 };
