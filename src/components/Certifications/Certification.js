f711d13a6eca8ba6d7b1abd8f7c9732ea6c86cf4
cult_mentality <kavitkarswanand5@gmail.com>
2023-06-03 12:49:25 -0700

    basic formatting

--- src/components/Certifications/Certification.js
+++ src/components/Certifications/Certification.js
@@ -4,7 +4,7 @@ const Certification = (props) => {
   const { cert } = props;
 
   return (
-    <div className="flex justify-center items-center p-10 border border-gray-200 shadow-xl sm:w-5/6 md:w-4/5 lg:w-1/2">
+    <div className="flex justify-center items-center px-10 py-7 border border-gray-200 shadow-xl sm:w-5/6 md:w-4/5 lg:w-1/2">
       <img src={medal} className="w-14 h-14" />
       <div className="flex flex-col text-xl flex-1 font-bold items-center">
         <div className="text-center px-5">{cert.title}</div>
