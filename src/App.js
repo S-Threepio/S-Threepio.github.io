86b4109667ee7304d8ed2c803cebfb05b11a3077
cult_mentality <kavitkarswanand5@gmail.com>
2023-06-06 21:55:35 -0700

    added gap for browser bar

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
   return (
     <GlobalDataProvider>
       <Router>
-        <div className="flex flex-col h-screen m-0 p-0">
+        <div className="flex flex-col h-[calc(100vh-85px)] md:h-screen m-0 p-0">
           <Header />
           <div className="w-screen h-fit md:h-full flex flex-col overflow-y-scroll overflow-x-hidden sm:overflow-y-hidden scrollbar-hide p-0">
             <Routes>
