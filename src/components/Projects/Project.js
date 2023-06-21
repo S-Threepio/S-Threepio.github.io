32bd5df8f734c3aaee4bc1bdec5115e6a84ff5a2
cult_mentality <kavitkarswanand5@gmail.com>
2023-06-21 01:17:00 -0700

    fixed animations

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -5,32 +5,34 @@ const Project = (props) => {
   let visible = filter == "all" || filter == data.type.toLowerCase();
   return (
     <div
-      className={`transition-opacity duration-500 ease-in-out ${
-        visible ? "" : "hidden"
-      }  flex flex-col items-start m-1 md:scale-[95%] bg-primary border border-gray-300 shadow-lg`}
+      className={`transition-opacity duration-700 ease-in-out my-2 md:my-0
+      ${visible ? "opacity-100 h-auto" : "opacity-0 "}
+        flex flex-col items-start md:scale-[95%] bg-primary border border-gray-300 shadow-lg`}
     >
-      <img
-        src={data.img}
-        alt="Project"
-        className="self-center object-fill h-56 p-2 px-6 w-120"
-      />
-      <div className="px-6 pt-4 text-xl font-semibold">
-        {data.title}{" "}
-        <a href={data.link} className="font-bold underline text-blue-950 ">
-          [Github]
-        </a>
+      <div className={`flex  ${visible ? "flex-col" : "hidden"}`}>
+        <img
+          src={data.img}
+          alt="Project"
+          className="self-center object-fill h-56 p-2 px-6 w-120"
+        />
+        <div className="px-6 pt-4 text-xl font-semibold">
+          {data.title}{" "}
+          <a href={data.link} className="font-bold underline text-blue-950 ">
+            [Github]
+          </a>
+        </div>
+        <div className="px-6 font-semibold">{data.year}</div>
+        <div className="px-6 pt-4 font-serif text-base">
+          {data.tech.join(" - ")}
+        </div>
+        <ul className="px-6 py-4 text-lg">
+          {data.points.map((point, index) => (
+            <li key={index} className="p-1 list-disc">
+              {point}
+            </li>
+          ))}
+        </ul>
       </div>
-      <div className="px-6 font-semibold">{data.year}</div>
-      <div className="px-6 pt-4 font-serif text-base">
-        {data.tech.join(" - ")}
-      </div>
-      <ul className="px-6 py-4 text-lg">
-        {data.points.map((point, index) => (
-          <li key={index} className="p-1 list-disc">
-            {point}
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };
